8b1fc69d4394740a1f1ad01ef480bcfbafbf53ca
Benjamin Hoppe <hoppeconnect@gmail.com>
2020-07-17 06:49:28 -0400

    Initial Commit

---
+++ Grid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Grid
+//
+//  Created by Benjamin Hoppe on 2020-07-17.
+//  Copyright © 2020 Benjamin Hoppe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
